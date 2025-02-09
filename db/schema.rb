d2317be769fa45ac9754623ac42a1c52985b61b4
Luiz-Miotto <luiz.l.miotto94@gmail.com>
2025-02-09 18:11:27 -0500

    added test suite to app

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_20_202432) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_21_233031) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "published_at"
+    t.json "error_codes"
   end
 
   create_table "users", force: :cascade do |t|
